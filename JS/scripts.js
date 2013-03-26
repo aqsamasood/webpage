b1caee10203ebd098838392eab9e1c07e8bf74e3
Aqsa <aqsa@ophio.co.in>
2013-03-26 14:05:14 +0530

    javascript file is updated and form validation is done

--- JS/scripts.js
+++ JS/scripts.js
@@ -1,3 +1,4 @@
+
 var Site={
 	init:function(){
 		this.tabs();
@@ -21,9 +22,16 @@ var Site={
 		});
 	}
 }
-
-
+$.getJSON("https://api.twitter.com/1/statuses/user_timeline/aqsamasood.json?count=1&include_rts=1&callback=?", function(data) {
+	
+	
+     $("#tweet1").html(data[0].text+"<br>");
+     $("#tweet1").append(data[0].created_at);
+     $("#tweet2").html(data[0].text);
+     $("#tweet3").html(data[0].text);
+});
 $(document).ready(function(){
+	
 	Site.init();
 });
 
--- JS/validation.js
+++ JS/validation.js
@@ -1,21 +1,65 @@
-$("ul li:first-child").click(
-function()
-{
-$(this).show("#categories");
-})
-// show(var){
-// 	for (var i = arr.length - 1; i >= 0; i--) {
-// 		arr[i]
-// 	};
-// }
-function validateForm()
-{
-var x=document.forms["myForm"]["email"].value;
-var atpos=x.indexOf("@");
-var dotpos=x.lastIndexOf(".");
-if (atpos<1 || dotpos<atpos+2 || dotpos+2>=x.length)
-  {
-  alert("Not a valid e-mail address");
-  return false;
-  }
+function formValidator(){
+	// Make quick references to our fields
+	var name = document.getElementById('name');
+	var email = document.getElementById('email');
+	var projectDetails=document.getElementById('projectDetails');
+	var timeScale=document.getElementById('timeScale');
+	
+	// Check each input in the order that it appears in the form!
+	if(isAlphabet(name, "Please enter only letters for your name!")){
+						if(emailValidator(email, "Please enter a valid email address!")){
+							if(isProjectDetails(projectDetails, "Please enter project Details!")){
+								if(isTimescale(timeScale, "Please enter Time scales!")){				
+							      	return true;
+							    }
+							}
+						}
+	}
+	return false;
+	
+}
+
+function isAlphabet(name, msg){
+	var alphaExp = /^[a-zA-Z]+$/;
+	if(name.value.match(alphaExp)){
+		return true;
+	}else{
+		alert(msg);
+		name.focus();
+		return false;
+	}
+}
+
+function isProjectDetails(projectDetails, msg){
+	 var alphaExp = /^[a-zA-Z]+$/;
+	if(projectDetails.value.match(alphaExp)){
+		return true;
+	}else{
+		alert(msg);
+		projectDetails.focus();
+		return false;
+	}
+}
+
+function isTimescale(timeScale, msg){
+	// var alphaExp = /^[a-zA-Z]+$/;
+	if(timeScale){
+		return true;
+	}else{
+		alert(msg);
+		timeScale.focus();
+		return false;
+	}
+}
+
+
+function emailValidator(elem, msg){
+	var emailFilter = /^[\w\-\.\+]+\@[a-zA-Z0-9\.\-]+\.[a-zA-z0-9]{2,4}$/;
+	if(elem.value.match(emailFilter)){
+		return true;
+	}else{
+		alert(msg);
+		elem.focus();
+		return false;
+	}
 }
